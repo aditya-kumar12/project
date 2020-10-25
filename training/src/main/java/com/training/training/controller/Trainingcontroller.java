1be6396501754b231447367cd3e1d6517f51d74d
aditya <adityakumarst12@gmail.com>
2020-10-25 18:48:12 +0530

    zuul and eureka done

--- training/src/main/java/com/training/training/controller/Trainingcontroller.java
+++ training/src/main/java/com/training/training/controller/Trainingcontroller.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/training")
+//@RequestMapping("/training")
 public class Trainingcontroller {
 
     @Autowired
