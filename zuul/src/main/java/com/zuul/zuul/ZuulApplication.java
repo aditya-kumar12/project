0fe30d6a1b2d138dc1a9cd3125046ba3fc95e8de
aditya <adityakumarst12@gmail.com>
2020-11-04 12:19:55 +0530

    token done

--- zuul/src/main/java/com/zuul/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/zuul/zuul/ZuulApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
